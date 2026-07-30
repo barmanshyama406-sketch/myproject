b9c193d9b70d5db61128435c30d6af88a5cd16b4
Shyampada Barman <barmanshyama406@gmail.com>
2026-07-30 10:32:46 +0530

    Connect Firebase

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const chat = document.getElementById("chat");
-const message = document.getElementById("message");
-const sendBtn = document.getElementById("sendBtn");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onChildAdded
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-database.js";
 
-sendBtn.addEventListener("click", () => {
-  if (message.value.trim() === "") return;
+const firebaseConfig = {
+  apiKey: "AIzaSyDHuie3BYhnsG8XgXP_Dn3-fmqrOq0UhCA",
+  authDomain: "simple-chat-29379.firebaseapp.com",
+  databaseURL: "https://simple-chat-29379-default-rtdb.firebaseio.com",
+  projectId: "simple-chat-29379",
+  storageBucket: "simple-chat-29379.firebasestorage.app",
+  messagingSenderId: "110265526293",
+  appId: "1:110265526293:web:bd5796795b1beb1c32061c"
+};
 
-  chat.innerHTML += `<p><b>You:</b> ${message.value}</p>`;
-  message.value = "";
-  chat.scrollTop = chat.scrollHeight;
-});
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+console.log("Firebase Connected");
